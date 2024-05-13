e9265692ed8129edc993676bb8c2c2310b422997
DiegoEngenheiro <diegogueta@gmail.com>
2024-05-13 10:46:11 -0300

    Publicação
    commit

---
+++ clientes/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Cliente
+import re
+from django.core import serializers
+import json
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+
+
+def pagina_inicial(request):
+    return render(request, 'login.html')  
+
+
+def clientes(request):
+    if request.method == "GET":
+        clientes_list = Cliente.objects.all()
+        return render(request, 'clientes.html', {'clientes': clientes_list})
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        
+
+        cliente = Cliente.objects.filter(cpf=cpf)
+
+        if cliente.exists():
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'email': email, '': zip() })
+
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf, '': zip()})
+
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+
+        cliente.save()
+
+        return HttpResponse('Teste')       
+
+
+def att_cliente(request):
+    id_cliente = request.POST.get('id_cliente')
+    cliente = Cliente.objects.filter(id=id_cliente)
+    cliente_json = json.loads(serializers.serialize('json', cliente))[0]['fields']
+    cliente_id = json.loads(serializers.serialize('json', cliente))[0]['pk']
+    data = {'cliente': cliente_json, 'cliente_id': cliente_id}
+    return JsonResponse(data)
+
+
+def update_cliente(request, id):
+    body = json.loads(request.body)
+
+    nome = body['nome']
+    sobrenome = body['sobrenome']
+    email = body['email']
+    cpf = body['cpf']
+
+    cliente = get_object_or_404(Cliente, id=id)
+    try:
+        cliente.nome = nome
+        cliente.sobrenome = sobrenome
+        cliente.email = email
+        cliente.cpf = cpf
+        cliente.save()
+        return JsonResponse({'status': '200', 'nome': nome, 'sobrenome': sobrenome, 'email': email, 'cpf': cpf})
+    except:
+        return JsonResponse({'status': '500'})
+
